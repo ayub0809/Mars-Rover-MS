887da427c36bbbae4855355101d3f36273697a87
ayushi <ayub0809@gmail.com>
2020-07-24 05:01:12 +0430

    Updates

---
+++ precache-manifest.eea765f48c078b59602e82335da0b07c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2443fc7465fd63a113a1fd6b8c63944f",
+    "url": "/mars-rover-ms/index.html"
+  },
+  {
+    "revision": "b3f9125d549b7bc351c0",
+    "url": "/mars-rover-ms/static/css/main.59b2fd79.chunk.css"
+  },
+  {
+    "revision": "b2825a44575a76b6be2f",
+    "url": "/mars-rover-ms/static/js/2.6831fd0b.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/mars-rover-ms/static/js/2.6831fd0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3f9125d549b7bc351c0",
+    "url": "/mars-rover-ms/static/js/main.b19615d6.chunk.js"
+  },
+  {
+    "revision": "7ec93337917bb06d9baa",
+    "url": "/mars-rover-ms/static/js/runtime-main.352f8827.js"
+  },
+  {
+    "revision": "dc205653b1e934cca65713304bab7d01",
+    "url": "/mars-rover-ms/static/media/mars_rover.dc205653.png"
+  }
+]);
